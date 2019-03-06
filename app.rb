f297e6317c48080b970f1ceb05e3ba0ca45766e8
Natalie Halawani <hala1224@gmail.com>
2019-03-06 23:51:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,13 +3,15 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
+ # code other routes/actions here
     get '/' do
       erb :root
     end
 
     get '/new' do
-      erb :'pirates/new'
+      erb :"pirates/new"
     end
+
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
