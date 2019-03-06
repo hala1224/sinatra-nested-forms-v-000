3c809ca40a209285cbb6d8bf867639a5b5a9130c
Natalie Halawani <hala1224@gmail.com>
2019-03-06 23:28:11 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,5 +1,20 @@
 class Ship
   attr_accessor :name, :type, :booty
   
-  def initialize(arg)
+  SHIPS=[]
+  def initialize(args)
+    @name=args[:name]
+    @type=args[:type]
+    @booty=args[:booty]
+    
+    SHIPS << self
+  end
+  
+  def self.all
+    SHIPS
+  end
+  
+  def self.clear
+    
+  end
 end
